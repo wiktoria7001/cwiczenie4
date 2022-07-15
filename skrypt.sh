e45a13cff0f4791b1dc21bdd67791d045fd395e4
Viktoryia Semianiuk <wiktoria7001@gmail.com>
2022-07-15 19:00:40 +0200

    Skrypt tworzy 100 plikow

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,11 @@ if [ "$1" == --date ]; then
   date
 fi
 
+if [ "$@" == "--logs" ]; then
+  for i in `seq -w 1 100`
+    do
+    echo "log$i.txt" > "log$i.txt"
+    echo "$0" >> "log$i.txt"
+    date >> "log$i.txt"
+  done
+fi
